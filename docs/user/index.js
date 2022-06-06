9f739777a84be273224e552420c607713f7fa921
alaa <alaakhaled5588@gmail.com>
2022-06-06 18:30:06 +0200

    add login endpoint

--- docs/user/index.js
+++ docs/user/index.js
@@ -1,6 +1,6 @@
 import { usersTag } from '../tags.js'
 import { verificationTokenParameter } from './parameters.js'
-import { signupRequestBodySchema, signupResponseBodySchema } from './schema.js'
+import { loginRequestBodySchema, loginResponseBodySchema, signupRequestBodySchema, signupResponseBodySchema } from './schema.js'
 
 export const usersDocs = {
 
@@ -52,6 +52,46 @@ export const usersDocs = {
 
         404: {
           description: 'Not found token'
+        }
+      }
+
+    }
+  },
+
+  '/users/login': {
+    post: {
+      tags: [usersTag.name],
+      description: 'Login users',
+
+      requestBody: {
+        content: {
+          'application/json': {
+            schema: loginRequestBodySchema
+          }
+        }
+      },
+
+      responses: {
+        200: {
+          description: 'User login successfully',
+          content: {
+            'application/json': {
+              schema: loginResponseBodySchema
+            }
+          }
+        },
+
+        400: {
+          description: 'Bad Request, either from the schema of the request body or mongoose'
+        },
+        404: {
+          description: 'User not found'
+        },
+        403: {
+          description: 'Wrong password'
+        },
+        401: {
+          description: 'The status is pending'
         }
       }
 
--- docs/user/schema.js
+++ docs/user/schema.js
@@ -48,3 +48,34 @@ export const signupResponseBodySchema = {
 
   }
 }
+
+export const loginRequestBodySchema = {
+  type: 'object',
+  required: ['username', 'password', 'email'],
+  properties: {
+    username: {
+      type: 'string',
+      example: 'Someone-cool96'
+    },
+    email: {
+      type: 'string',
+      example: 'someone5588@gmail.com'
+    },
+    password: {
+      type: 'string',
+      example: 'L3L311235813'
+    }
+
+  }
+}
+export const loginResponseBodySchema = {
+  type: 'object',
+  required: ['verificationToken'],
+  properties: {
+    verificationToken: {
+      type: 'string',
+      description: 'JWT for this specific user'
+    }
+
+  }
+}
