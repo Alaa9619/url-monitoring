feec22306e249ddb47807f6d63dc72718147740d
alaa <alaakhaled5588@gmail.com>
2022-06-06 15:43:51 +0200

    add errorhandler middleware

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import swaggerUi from 'swagger-ui-express'
 import httpStatus from 'http-status'
 import { swaggerDocument } from '../docs/config.js'
 import appRouter from './router.js'
+import { errorHandler } from '../middleware/error-handler.js'
 
 const { NOT_FOUND } = httpStatus
 
@@ -18,7 +19,7 @@ app.use(express.json())
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(appRouter)
 
-// app.use(errorHandler);
+app.use(errorHandler)
 
 app.use((req, res) => {
   res.status(NOT_FOUND).json({ message: `Cannot ${req.method} ${req.originalUrl}` })
