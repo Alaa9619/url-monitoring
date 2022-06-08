9cd1eabfe9c17d7d58bb38fae867ba0dbc0377af
alaa <alaakhaled5588@gmail.com>
2022-06-08 16:08:27 +0200

    add url-check router

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import UserRouter from './modules/users/router.js'
+import urlChecksRouter from './modules/url-checks/router.js'
 
 const router = new Router()
 
 router.use('/users', UserRouter)
+router.use('/url-checks', urlChecksRouter)
 
 export default router
