21c3fb43bc4be94ff79a3b3eb56750d103a9d170
alaa <alaakhaled5588@gmail.com>
2022-06-09 16:25:59 +0200

    add controllers for get , post , Delete

--- src/modules/url-checks/controller.js
+++ src/modules/url-checks/controller.js
@@ -1,7 +1,7 @@
 import httpStatus from 'http-status'
 import { urlChecksServices } from './services.js'
 
-const { CREATED } = httpStatus
+const { CREATED, OK, NO_CONTENT } = httpStatus
 
 export const urlChecksController = {
   createUrlChecksInstance: async (req, res, next) => {
@@ -14,6 +14,39 @@ export const urlChecksController = {
     } catch (error) {
       return next(error)
     }
-  }
+  },
 
+  getUrlChecksInstance: async (req, res, next) => {
+    const { params: { id: urlChecksInstanceId }, user: { _id: userId } } = req
+
+    try {
+      const urlChecksInstance = await urlChecksServices.getUrlChecksInstance({ urlChecksInstanceId }, { userId })
+
+      return res.status(OK).send(urlChecksInstance)
+    } catch (error) {
+      return next(error)
+    }
+  },
+
+  getBulkUrlsChecks: async (req, res, next) => {
+    const { query: { tags }, user: { _id: userId } } = req
+    try {
+      const urlChecksBulk = await urlChecksServices.getBulkUrlsChecks({ tags }, { userId })
+
+      return res.status(OK).send(urlChecksBulk)
+    } catch (error) {
+      return next(error)
+    }
+  },
+
+  deleteUrlChecksInstance: async (req, res, next) => {
+    const { params: { id: urlChecksInstanceId }, user: { _id: userId } } = req
+    try {
+      await urlChecksServices.deleteUrlChecksInstance({ urlChecksInstanceId }, { userId })
+
+      return res.status(NO_CONTENT).send()
+    } catch (error) {
+      return next(error)
+    }
+  }
 }
