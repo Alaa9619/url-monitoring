bc58e807777af2d56e471e943c317287672f7916
alaa <alaakhaled5588@gmail.com>
2022-07-02 10:12:02 +0200

    add url checkes tags

--- docs/index.js
+++ docs/index.js
@@ -1,7 +1,9 @@
+import { urlChecksDocs } from './url-checks/index.js'
 import { usersDocs } from './user/index.js'
 
 export const modulesDocs = {
 
-  ...usersDocs
+  ...usersDocs,
+  ...urlChecksDocs
 
 }
--- docs/tags.js
+++ docs/tags.js
@@ -3,8 +3,13 @@ export const usersTag = {
   name: 'Users',
   description: 'Users endpoints'
 }
+export const urlChecksTag = {
+  name: 'Url-Checks',
+  description: 'Url-Checks'
+}
 
 export const tags = [
-  usersTag
+  usersTag,
+  urlChecksTag
 
 ]
